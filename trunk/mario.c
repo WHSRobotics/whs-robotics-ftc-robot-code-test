afe2b56ad6175661fe98ed749d2339a8328faefc
phreonx13@gmail.com <phreonx13@gmail.com@218ed165-06cc-0d0c-b384-34b2120dbbdb>
2012-11-26 01:00:42 +0000

    raised octave and changed notes in mario.c

--- trunk/mario.c
+++ trunk/mario.c
@@ -1,20 +1,20 @@
 /*
-Q-A3, Q-G3, rest, Q-G3
-H.-G3, H.-F3, H.-E3
-Q-D3, Q-E2, Q-D2, Q-E2
-Q-C2*/
+Q-B4, Q-F5, rest, Q-F5
+20-F5, 20-E5, 20-D5
+Q-C5, Q-E4, Q-G4, Q-E4
+Q-C4*/
 
-const float C3 = 130.81;
-const float D3 = 146.83;
-const float E3 = 164.81;
-const float D4 = 293.67;
+const float B4 = 493.88;
+const float F5 = 698.46;
+const float E5 = 659.26;
+const float D5 = 587.33;
+const float C5 = 523.25;
 const float E4 = 329.63;
-const float F4 = 349.23;
 const float G4 = 392.00;
-const float A4 = 440.00;
+const float C4 = 261.63;
 
 const int HALF = 30;
-const int HALF_DOT = 45;
+const int HALF_TUP = 20;
 const int QUARTER = 15;
 
 void playNote(float freq, int length)
@@ -25,20 +25,20 @@ void playNote(float freq, int length)
 
 task main()
 {
-  playNote(A4, QUARTER);
-  playNote(G4, QUARTER);
+  playNote(B4, QUARTER);
+  playNote(F5, QUARTER);
   wait10Msec(QUARTER);
+  playNote(F5, QUARTER);
+
+  playNote(F5, HALF_TUP);
+  playNote(E5, HALF_TUP);
+  playNote(D5, HALF_TUP);
+
+  playNote(C5, QUARTER);
+  playNote(E4, QUARTER);
   playNote(G4, QUARTER);
+  playNote(E4, QUARTER);
 
-  playNote(G4, HALF_DOT);
-  playNote(F4, HALF_DOT);
-  playNote(E4, HALF_DOT);
-
-  playNote(D4, QUARTER);
-  playNote(E3, QUARTER);
-  playNote(D3, QUARTER);
-  playNote(E3, QUARTER);
-
-  playNote(C3, QUARTER);
+  playNote(C4, QUARTER);
 
 }
