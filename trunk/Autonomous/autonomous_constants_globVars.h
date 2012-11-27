405cb9079592519e81121a4f8c68c4a01100c803
phreonx13@gmail.com <phreonx13@gmail.com@218ed165-06cc-0d0c-b384-34b2120dbbdb>
2012-11-27 03:17:16 +0000

    general cleaning up in autonomous: added variables, comments, and functions for readability

--- trunk/Autonomous/autonomous_constants_globVars.h
+++ trunk/Autonomous/autonomous_constants_globVars.h
@@ -3,11 +3,15 @@
 
 ///////////////////KONSTANTS///////////////////
 
-const float LOW_TURK = 0.75;
-const float MID_TURK = 0.9;
-const float HIGH_TURK = 1.0;
-const float ADJUST_M = 1.0;
-const float ADJUST_B = 7.0;
+const float LOW_TURK = 0.75; //turn konstant for low power
+const float MID_TURK = 0.9; //turn konstant for mid power
+const float HIGH_TURK = 1.0; //turn konstant for high power
+const float ADJUST_M = 1.0; //slope of linear equation to scale target angle
+const float ADJUST_B = 7.0; //y-intercept of linear equation to scale target angle
+
+const float MILLISECOND = 1000.0; //number of milliseconds in one second
+const float DEFAULT_VAL = 42.0; //generic value for initialization that's greater than 0
+
 
 ///////////////GLOBAL VARIABLES////////////////
 
@@ -15,7 +19,7 @@ float currentValue = 0.0;
 float angleChange = 0.0;
 float timeChange = 0.0;
 float currTotalMove = 0.0;
-float remainingTurn = 42.0;
+float remainingTurn = DEFAULT_VAL;
 float error = 0.0;
 float gyroReading = 0.0;
 float adjustedTarget = 0.0;
