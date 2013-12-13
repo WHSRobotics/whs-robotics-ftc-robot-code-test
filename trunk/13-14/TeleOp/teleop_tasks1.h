178d1309560a227e3ff5594048f7ac29bfcbb0ac
phreonx13@gmail.com <phreonx13@gmail.com@218ed165-06cc-0d0c-b384-34b2120dbbdb>
2013-12-13 23:38:51 +0000

    mostly teleop stuff including flag

--- trunk/13-14/TeleOp/teleop_tasks1.h
+++ trunk/13-14/TeleOp/teleop_tasks1.h
@@ -8,10 +8,22 @@
 #include "teleop_globVars1.h";
 
 
+
 /////////////FUNCTIONS//////////////
 /*Initialization*/
 void InitializeRobot()
 {
+	//ready KONSTANTS
+	const int FLAG_READY = 120;
+	const int AUTO_INIT = 15;
+	const int LEFT_HANG_INIT = 60;
+	const int RIGHT_HANG_INIT = 180;
+
+	//set to ready positions
+	servo[flagServo] = FLAG_READY;
+	servo[autoServo] = AUTO_INIT;
+	servo[hangServo1] = LEFT_HANG_INIT;
+	servo[hangServo2] = RIGHT_HANG_INIT;
 	return;
 }
 
@@ -114,16 +126,28 @@ task Arm()
 ///////////////////////////////////////////
 task DriveTrain()
 {
+	//Drive Train KONSTANTS
+	const int HI_THRESH = 25; //high joy threshold
+	const int LOW_THRESH = 15; //low joy threshold
+	const int JOY_SCALE = 25/32; //scale from joystick val to motor power
+
+	int leftPow = 0;
+	int rightPow = 0;
+
 	while(true)
 	{
 		//high priority
 		//-----------joystick 1------------//
 		getJoystickSettings(joystick);
-		if(abs(joystick.joy1_y1 * 25/32) > DRIVE_LOW_THRESH || abs(joystick.joy1_y2 * 25/32) > DRIVE_LOW_THRESH)
+
+		leftPow = joystick.joy1_y1 * JOY_SCALE;
+		rightPow = joystick.joy1_y2 * JOY_SCALE;
+
+		if(abs(leftPow) > LOW_THRESH || abs(rightPow) > LOW_THRESH)
 		//if movement is significant
 		{
-			motor[leftDrive] = joystick.joy1_y1 * 25/32;
-			motor[rightDrive] = joystick.joy1_y2 *25/32;
+			motor[leftDrive] = joystick.joy1_y1 * JOY_SCALE;
+			motor[rightDrive] = joystick.joy1_y2 * JOY_SCALE;
 		}
 		else //if movement is insignificant
 		{
@@ -136,10 +160,16 @@ task DriveTrain()
 ///////////////////////////////////////////
 //////-------------HANG-------------///////
 // Button presses drive motors at set speeds.
-// USED: Btn 1 (FS, Raise), TopHat 0 (D-UP, FS, Lift)
+// USED: Btn 1 (FS, Raise)
 ///////////////////////////////////////////
 task Hang()
 {
+	//Hang Servo Positions
+	const int POS_ONE = 60;
+	const int END_ONE = 250;
+	const int POS_TWO = 180;
+	const int END_TWO = 0;
+
 	while(true)
 	{
 		getJoystickSettings(joystick);
@@ -157,14 +187,14 @@ task Hang()
 		//Only when the button for the lift arms are pressed, lift
 		if(joy1Btn(1) && joy2Btn(1))
 		{
-			servoTarget[hangServo1] = HANG_END_ONE;
-			servoTarget[hangServo2] = HANG_END_TWO;
+			servoTarget[hangServo1] = END_ONE;
+			servoTarget[hangServo2] = END_TWO;
 		}
 		//Otherwise, Drop the servo arms
 		else
 		{
-			servoTarget[hangServo1] = HANG_POS_ONE;
-			servoTarget[hangServo2] = HANG_POS_TWO;
+			servoTarget[hangServo1] = POS_ONE;
+			servoTarget[hangServo2] = POS_TWO;
 		}
 	}
 }
@@ -202,112 +232,58 @@ task Intake()
 }
 
 
-<<<<<<< .mine
-////////////////////////////////////
-//////----------FLAG----------//////
-////////////////////////////////////
+///////////////////////////////////////////
+//////------------INTAKE------------///////
+// Button presses drive motors at set speeds.
+// USED: Btn 5 (eat/drop), Btn 2 (Toggle Intake)
+///////////////////////////////////////////
 task Flag()
 {
+	///////Flag Stages///////
+	const int STG_READY = 0;
+	const int STG_SET = 1;
+	const int STG_GO = 2;
+	///////Flag Positions///////
+	const int POS_READY = 100;
+	const int POS_SET = 50;
+	const int POS_GO = 0;
+
+	int currStage = STG_READY;
 	while(true)
 	{
 		getJoystickSettings(joystick);
-		if(joy1Btn(4) && joy2Btn(4))
+
+		if(currStage == STG_READY)
 		{
-			servo[flagServo] = 65;
+			if(joy1Btn(4) && joy2Btn(4))
+			{
+				servo[flagServo] = POS_SET;
+				currStage = STG_SET;
+			}
+			else
+			{
+				servo[flagServo] = POS_READY;
+			}
+		}
+		else if(currStage == STG_SET)
+		{
+			if(joy1Btn(3) && joy2Btn(3))
+			{
+				servo[flagServo] = POS_GO;
+				currStage = STG_GO;
+			}
+			else
+			{
+				servo[flagServo] = POS_SET;
+			}
 		}
 		else
 		{
-			servo[flagServo] = 0;
+			servo[flagServo] = POS_GO;
 		}
 	}
 }
 
 
-//----NOT TO BE USED FOR COMPETITION 1---//
 
-///////////////////////////////////////////
-//////----------TURNTABLE-----------///////
-// D-pad drives motor at set speed.
-// USED: TopHat 2 (right), TopHat 4 (left)
-//       Btn 5 (set slow speed)
-///////////////////////////////////////////
-/*task Turntable()
-{
-	while(true)
-	{
-		getJoystickSettings(joystick);
-
-		//-----------joystick 1------------//
-		//low priority
-		if(joystick.joy1_TopHat == 2 && !(joystick.joy1_TopHat == 4)) //turntable right
-		{
-			if(!joy2Btn(5)) //regular speed
-			{
-				motor[tableMotor] = TABLE_HI_POW;
-			}
-			else //slow speed
-			{
-				motor[tableMotor] = TABLE_LOW_POW;
-			}
-		}
-		else //if no top hat input
-		{
-			motor[tableMotor] = STOP;
-		}
-		if(joystick.joy1_TopHat == 4 && !(joystick.joy1_TopHat == 2)) //turntable left
-		{
-			if(!joy2Btn(5)) //regular speed
-			{
-				motor[tableMotor] = TABLE_HI_POW;
-			}
-			else //slow speed
-			{
-				motor[tableMotor] = TABLE_LOW_POW;
-			}
-		}
-		else //if no top hat input
-		{
-			motor[tableMotor] = STOP;
-		}
-
-		//-----------joystick 2------------//
-		//high priority
-		if(joystick.joy2_TopHat == 2 && !(joystick.joy2_TopHat == 6)) //turntable right
-		{
-			if(!joy2Btn(5)) //regular speed
-			{
-				motor[tableMotor] = TABLE_HI_POW;
-			}
-			else //slow speed
-			{
-				motor[tableMotor] = TABLE_LOW_POW;
-			}
-		}
-		else //if no top hat input
-		{
-			motor[tableMotor] = STOP;
-		}
-
-		if(joystick.joy2_TopHat == 6 && !(joystick.joy2_TopHat == 2)) //turntable left
-		{
-			if(!joy2Btn(5)) //regular speed
-			{
-				motor[tableMotor] = -TABLE_HI_POW;
-			}
-			else //slow speed
-			{
-				motor[tableMotor] = -TABLE_LOW_POW;
-			}
-		}
-		else //if no top hat input
-		{
-			motor[tableMotor] = STOP;
-		}
-	}
-}*/
-
-
-
-=======
->>>>>>> .r105
 #endif;
