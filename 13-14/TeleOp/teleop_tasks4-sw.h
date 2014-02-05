9f2660dc9ad02630386b607a1d0347c8091195e5
momopeachboy026 <momopeachboy026@gmail.com>
2014-02-05 04:21:21 +0000

    Edited Teleop with drive functions. Have not be tested yet. We still do have to make a new variables tele-op file (which I might probably take care of)

--- 13-14/TeleOp/teleop_tasks4-sw.h
+++ 13-14/TeleOp/teleop_tasks4-sw.h
@@ -9,9 +9,18 @@ ratchetDisable = servo
 
 #include "JoystickDriver.c"
 
+const int HI_THRESH = 25;
+const int LOW_THRESH = 15;
+const float JOY_MAP = 0.78125;
+const float SERVO_MAP = 255.0/PI;
+const float ROT_SCALE = 0.075;
+const float TANK_SPEED_SCALE = 1.0;
+const float SWERVE_SPEED_SCALE = 1.0;
+const float HALF_WIDTH_X = 6.8125;
+const float HALF_LENGTH_Y = 7.125;
+
 bool intakeOn = false;
 bool intakeReversed = false;
-
 
 /*
 void runIntake(float motPow)
@@ -20,6 +29,112 @@ void runIntake(float motPow)
 	motor[intakeR] = motPow;
 }*/
 
+////////----FUNCTIONS----////////
+
+float magnitudeCalc(float inputX, float inputY)
+{
+	return sqrt( pow(inputX, 2) + pow(inputY, 2) ) < 128.0
+	? sqrt( pow(inputX, 2) + pow(inputY, 2) )
+	: 128.0;
+}
+
+void piMotor(tMotor motorName, TServoIndex servoName, float inputY, float inputX, int initServoPos)
+{
+	if((magnitudeCalc(inputY, inputX) > LOW_THRESH) && (magnitudeCalc(inputY, inputX) <= 128.0))
+	{
+		if(atan2(inputY, inputX) < 0)
+		{
+			servo[servoName] = (atan2(inputY, inputX) + PI) * SERVO_MAP + initServoPos;
+			motor[motorName] = -magnitudeCalc(inputY, inputX) * JOY_MAP;
+		}
+		else
+		{
+			servo[servoName] = atan2(inputY, inputX) * SERVO_MAP + initServoPos;
+			motor[motorName] = magnitudeCalc(inputY, inputX)* JOY_MAP;
+		}
+	}
+	else
+	{
+		servo[servoName] = PI/2.0 * SERVO_MAP + initServoPos;
+		motor[motorName] = 0;
+	}
+}
+
+void assistedTankControl(float diffY1Input, float diffY2Input)
+{
+	float scaledY1 = -diffY1Input * TANK_SPEED_SCALE;
+	float scaledY2 = -diffY2Input * TANK_SPEED_SCALE;
+	float velX = HALF_LENGTH_Y * (scaledY1 - scaledY2)/(2.0*HALF_WIDTH_X);
+
+	piMotor(sweFL, swiFL, scaledY1, -velX, 0);
+	piMotor(sweBL, swiBL, scaledY1, velX, 0);
+	piMotor(sweFR, swiFR, scaledY2, -velX, 0);
+	piMotor(sweBR, swiBR, scaledY2, velX, 0);
+}
+
+oid swerveControl(float transYInput, float transXInput, float angularInput)
+{
+	float angSclr = abs(angularInput) > HI_THRESH
+	? angularInput * ROT_SCALE
+	: 0.0 ;
+	float transXSclr = magnitudeCalc(transXInput, transYInput) > LOW_THRESH
+	? transXInput * SWERVE_SPEED_SCALE
+	: 0.0 ;
+	float transYSclr = magnitudeCalc(transXInput, transYInput) > LOW_THRESH
+	? transYInput * SWERVE_SPEED_SCALE
+	: 0.0 ;
+
+	float velFLX = angSclr * -HALF_LENGTH_Y + transXSclr;
+	float velFLY = angSclr * -HALF_WIDTH_X + transYSclr;
+
+	float velBLX = angSclr * HALF_LENGTH_Y + transXSclr;
+	float velBLY = angSclr * -HALF_WIDTH_X +  transYSclr;
+
+	float velFRX = angSclr * -HALF_LENGTH_Y + transXSclr;
+	float velFRY = angSclr * HALF_WIDTH_X +  transYSclr;
+
+	float velBRX = angSclr * HALF_LENGTH_Y + transXSclr;
+	float velBRY = angSclr * HALF_WIDTH_X + transYSclr;
+
+	piMotor(sweFL, swiFL, -velFLY, velFLX, 0);
+	piMotor(sweBL, swiBL, -velBLY, velBLX, 0);
+	piMotor(sweFR, swiFR, -velFRY, velFRX, 0);
+	piMotor(sweBR, swiBR, -velBRY, velBRX, 0);
+}
+
+void driveSwitch()
+{
+	bool toggle = true;
+	while(true)
+	{
+		getJoystickSettings(joystick);
+		if(joy1Btn(9))
+		{
+			toggle = !toggle;
+			wait10Msec(25);
+		}
+		if(toggle)
+		{
+			swerveControl(joystick.joy1_y1, joystick.joy1_x1, joystick.joy1_x2);
+		}
+		else
+		{
+			assistedTankControl(joystick.joy1_y1, joystick.joy1_y2);
+		}
+	}
+}
+
+////////----TASKS----////////
+
+/**********DRIVE TRAIN********
+Controls 4 Servos and 4 Motors
+Joystick 1 and 2
+Btn 9 = toggle drive style
+*****************************/
+task Drivetrain()
+{
+	driveSwitch();
+}
 
 /**********SCORING ARM********
 Powered by 1 DC Motor
